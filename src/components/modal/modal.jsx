cfc25c3ce2be375f5327d90aa92a32ed307d849b
younisdev <younisayoub10@gmail.com>
2026-04-19 01:36:17 +0300

    Polished 2nd layer and expanded on extractcssclass on the cache utilities

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -12,7 +12,7 @@ import {
   ExecuteValidator,
   ExecuteRegex
 } from './dependencies/validator/validators';
-import { cachelayerThree, CACHETYPE } from '../../utils/Smart Json Caching/SJCManager';
+import { SJCManager, CACHETYPE } from '../../utils/Smart Json Caching/SJCManager';
 import React from 'react';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
@@ -69,7 +69,7 @@ function Modal({
     onSubmit
   );
   const modalRef = React.useRef(null);
-  cachelayerThree("../../components/modal/dependencies/themes.json" ,"../../components/modal/dependencies/style/themes.css", CACHETYPE.CSS);
+  console.log(SJCManager("../../components/modal/dependencies/themes.json" ,"../../components/modal/dependencies/style/themes.css", CACHETYPE.CSS, "Modal", "theme", "Industrial", "class"));
   function handleInputChange(name, value) {
     const validation = handleValidation();
     const nextData = { ...data, [name]: value };
--- src/utils/Smart Json Caching/SJCManager.js
+++ src/utils/Smart Json Caching/SJCManager.js
@@ -4,28 +4,79 @@ import Version from "../../../package.json"
 export const CACHETYPE = { CSS: 'css', ANIMATION: 'animation' };
 const VERSION = Version['version'];
 
-export async function cachelayerThree(jsonpath, csspath, type) {
-  const key = generateCacheKey(3, "Modal", "theme");
 
-  if(!localStorage.getItem(key)) return true;
+export async function SJCManager(jsonpath, csspath, type, component, utility, jsonKey, jsonclasskey="") {
+  const layerOneResult = null; // plug the actual func
+  if (layerOneResult !== null) return layerOneResult;
+  const layerTwoResult = null; // plug the actual func
+  if (layerTwoResult !== null) return layerTwoResult;
+  const layerThreeResult = await cachelayerThree(jsonpath, csspath, type, 3, component, utility, jsonKey, jsonclasskey);
+  return layerThreeResult;
+}
 
-  let rawJSON = await extractFile(jsonpath);;
+async function cachelayerThree(jsonpath, csspath, type, layer, component, utility, jsonKey, jsonclasskey) {
+ const key = generateCacheKey(3, "Modal", "theme");
+
+  if(localStorage.getItem(key)) return JSON.parse(localStorage.getItem(key));;
+
+  const rawJSONText = await extractFile(jsonpath); 
+  const JsonArray = JSON.parse(rawJSONText);
+  const jsonResult = JsonArray.find(e => e[utility] === jsonKey);
   let rawCSS = null;
 
+  let cssResult = null;
   if (type === CACHETYPE.CSS) {
     rawCSS = await extractFile(csspath);
   }
 
   let value = {
     ...(rawCSS !== null && {"CSS": rawCSS}),
-    ...(rawJSON !== null && {"JSON": JSON.stringify(rawJSON)}),
+    ...(JsonArray !== null && {"JSON": JsonArray}),
   };
 
+  let result = {
+    ...(cssResult !== null && {"CSS": cssResult}),
+    ...(jsonResult !== null && {"JSON": jsonResult}),
+  }
   localStorage.setItem(key, JSON.stringify(value));
 
   return true;
 }
+function cachelayerTwo(type, classname = 'None', jsonpath) {
+ const key = generateCacheKey(2, component, utility);
 
+  if(localStorage.getItem(key)){
+    return JSON.parse(localStorage.getItem(key));
+  }
+
+  const rawJSONText = await extractFile(jsonpath); 
+
+  const JsonArray = JSON.parse(rawJSONText);
+  const jsonResult = JsonArray.find(e => e[utility] === jsonKey);
+  let cssResult = null;
+
+  if (type === CACHETYPE.CSS) {
+    cssResult = extractCSSClass(jsonResult[jsonclasskey], csspath)
+  }
+
+  let value = {
+    ...(cssResult !== null && {"CSS": cssResult}),
+    ...(jsonResult !== null && {"JSON": JSON.stringify(jsonResult)}),
+  };
+
+  localStorage.setItem(key, JSON.stringify(value));
+
+  return JSON.stringify(value);
+}
+
+function cachelayerOne(type, classname = 'None', jsonpath) {
+  extractCSSClass(
+    'dyvix-modal-ember',
+    '../../components/modal/dependencies/style/themes.css'
+  );
+  if (type === CACHETYPE.CSS) {
+  }
+}
 
 async function extractFile(path)
 {
@@ -44,28 +95,33 @@ function generateCacheKey(layer, component, utility) {
   return key;
 }
 
-export function cachelayerOne(type, classname = 'None', jsonpath) {
-  extractCSSClass(
-    'dyvix-modal-ember',
-    '../../components/modal/dependencies/style/themes.css'
-  );
-  if (type === CACHETYPE.CSS) {
-  }
-}
 
-async function extractCSSClass(classname, Csspath) {
-  try {
-    const module = await import(/* @vite-ignore */ `${Csspath}?raw`);
-    const rawCSS = module.default || module;
-    const lines = rawCSS.replace(/\s+/g, ' ').trim().split('}');
-    let block = '';
-    const matches = lines
-      .filter((val) => val.trim().includes(classname))
-      .map((block) => block.trim() + '}');
-    block = matches.join('\n\n');
-
-    return block;
-  } catch (error) {
-    console.log('DyvixUI Sys error');
+async function extractCSSClass(classname, Csspath=null, cssblock=null) {
+  const rawCSS;
+  if(csspath !== null)
+  {
+    try {
+      const module = await import(/* @vite-ignore */ `${Csspath}?raw`);
+      rawCSS = module.default || module;
+    } catch (error) {
+      console.log('DyvixUI Sys error');
+    }
   }
+  else if (cssblock !==null)
+  {
+    rawCSS = cssblock
+  }
+  else
+  {
+    return null;
+  }
+
+  const lines = rawCSS.replace(/\s+/g, ' ').trim().split('}');
+  let block = '';
+  const matches = lines
+    .filter((val) => val.trim().includes(classname))
+    .map((block) => block.trim() + '}');
+  block = matches.join('\n\n');
+
+  return block;
 }
