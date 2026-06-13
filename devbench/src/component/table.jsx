a903b37ea96e7b97e8a5372c4b7d33886c4923d2
younisdev <younisayoub10@gmail.com>
2026-06-13 22:28:43 +0300

    add table JSON schema mode via columns prop(header).

--- devbench/src/component/table.jsx
+++ devbench/src/component/table.jsx
@@ -10,31 +10,15 @@ import { DYVIX_GLOBAL_ANIMATION, DYVIX_GLOBAL_THEME } from 'dyvix-ui';
 export function TableTest() {
   return (
     <>
-      <DyvixTable theme="Neon" interactive striped>
-        <DyvixTableHeader>
-          <DyvixTableRow>
-            <DyvixTableHead>Vehicle</DyvixTableHead>
-            <DyvixTableHead>Class</DyvixTableHead>
-            <DyvixTableHead>Top Speed</DyvixTableHead>
-          </DyvixTableRow>
-        </DyvixTableHeader>
-        <DyvixTableBody>
-          <DyvixTableRow>
-            <DyvixTableCell>Nissan Skyline GT-R R34</DyvixTableCell>
-            <DyvixTableCell>Street</DyvixTableCell>
-            <DyvixTableCell>180</DyvixTableCell>
-          </DyvixTableRow>
-          <DyvixTableRow>
-            <DyvixTableCell>Porsche 911 GT3 RS</DyvixTableCell>
-            <DyvixTableCell>Track</DyvixTableCell>
-            <DyvixTableCell>193</DyvixTableCell>
-          </DyvixTableRow>
-          <DyvixTableRow>
-            <DyvixTableCell>Mazda RX-7 FD</DyvixTableCell>
-            <DyvixTableCell>Drift</DyvixTableCell>
-            <DyvixTableCell>160</DyvixTableCell>
-          </DyvixTableRow>
-        </DyvixTableBody>
+      <DyvixTable     
+        columns={[
+          { key: 'id', label: 'ID'},
+          { key: 'name', label: 'Name' },
+          { key: 'type', label: 'Type' },
+          { key: 'hp', label: 'HP' },
+          { key: 'region', label: 'Region' },
+        ]}>
+        
       </DyvixTable>
     </>
   );
--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -1,11 +1,27 @@
+import React from 'react';
 import './dependencies/style/style.css';
-
-function DyvixTable({ children, className = '' }) {
+import DyvixTableHeader from './DyvixTableHeader';
+import DyvixTableHead from './DyvixTableHead';
+import DyvixTableRow from './DyvixTableRow';
+function DyvixTable({ children, className = '', columns}) {
   const tableClasses = `dyvix-table ${className}`.trim();
   const props = {
     className: tableClasses
   };
 
+  const ConstructTable = () => (
+    <DyvixTableHeader>
+      <DyvixTableRow>
+        {columns.map((col, i) => {
+          return <DyvixTableHead key={col.key || i}>{typeof col === 'string' ? col: col.label}</DyvixTableHead>
+        })}
+      </DyvixTableRow>
+    </DyvixTableHeader>
+  );
+
+  const resultJSX = React.useMemo(() => columns ? ConstructTable(): null , [columns]);
+  children = children ? children: resultJSX;
+  console.log(children)
   return (
     <div className="dyvix-table-wrapper">
       <table {...props}>{children}</table>
